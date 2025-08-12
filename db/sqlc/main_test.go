df5f03808822073cec4390536f003b5f220ce9d7
Le Nguyen Hai Dang <danglnh07.work@gmail.com>
2025-08-12 18:17:22 +0700

    Fix error in handling fallback value in main_test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -23,14 +23,15 @@ func TestMain(m *testing.M) {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	// Load db config from .env
-	dbDriver, dbSource := "postgres", "postgresql://root:123456@localhost:5432/gobank?sslmode=disable"
+	dbDriver, dbSource := "", ""
 
 	err := godotenv.Load("../../.env") // Since this test is run from the ./db/sqlc/main_test.go
 	if err != nil {
 		logger.Error("Failed to load db config from main_test.go", "error", err)
+		dbDriver, dbSource = "postgres", "postgresql://root:123456@localhost:5432/gobank?sslmode=disable"
+	} else {
+		dbDriver, dbSource = os.Getenv("DB_DRIVER"), os.Getenv("DB_SOURCE")
 	}
-
-	dbDriver, dbSource = os.Getenv("DB_DRIVER"), os.Getenv("DB_SOURCE")
 
 	conn, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
